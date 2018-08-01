c800938c85fc7a238e95d1a664cb99774cf7d1b9
oshikzigler <oshikzigler@gmail.com>
2018-08-01 15:43:57 +0300

    Selenium commit

--- src/ObjectRepository.java
+++ src/ObjectRepository.java
@@ -19,12 +19,16 @@ public class ObjectRepository {
     public static By UserNameByXpath = By.xpath("//input[@name='username']");
     public static By PasswordByXpath = By.xpath("//input[@name='password']");
     public static By SubmitById = By.id("submit");
+    public static By LogoutById = By.id("logout");
 
     private static WebDriver driver;
-    public static Properties properties;
+    public static Properties pro;
 
     @BeforeClass
     public static void openBrowser() throws ParserConfigurationException, SAXException, IOException {
+
+        LoadProperties();
+
         System.setProperty("webdriver.chrome.driver", "/Users/oshikzigler/Documents/Automation/Selenium/Drivers/Chrome/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().fullscreen();
@@ -32,32 +36,46 @@ public class ObjectRepository {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
+    public static void LoadProperties() throws IOException{
+
+        File src = new File("Resources/ObjectRepositoryProperties.properties");
+        FileInputStream fis =new FileInputStream(src);
+        pro = new Properties();
+        pro.load(fis);
+    }
+
     @AfterClass
     public static void closeBrowser() {
+
         driver.quit();
     }
 
-    public static void LoadProperties() throws IOException{
-        File src = new File("ObjectRepository_Properties");
-        FileInputStream fileInputStream =new FileInputStream(src);
-        Properties pro = new Properties();
-        pro.load(fileInputStream);
-    }
 
     @Test
     public void Submit() {
-        //Usage of object repository
-        driver.findElement((ObjectRepository.UserNameByXpath)).sendKeys("Oshik");
-        driver.findElement((ObjectRepository.PasswordByXpath)).sendKeys("Pass123");
-        driver.findElement((ObjectRepository.SubmitById)).click();
+        //Usage of object repository - ERROR
+        driver.findElement(ObjectRepository.UserNameByXpath).sendKeys("oshik");
+        driver.findElement(ObjectRepository.PasswordByXpath).sendKeys("pass1234");
+        driver.findElement(ObjectRepository.SubmitById).click();
 
-        //Closing alert dialog
+        //Closing alert dialog and refreshing page
         Alert alert = driver.switchTo().alert();
         alert.accept();
         driver.navigate().refresh();
 
-        //Usage of properties
-        driver.findElement(By.xpath(properties.getProperty("demo.login.username"))).sendKeys("Oshik");
+        //Usage of object repository - SUCCESS
+        driver.findElement(ObjectRepository.UserNameByXpath).sendKeys("admin");
+        driver.findElement(ObjectRepository.PasswordByXpath).sendKeys("demo");
+        driver.findElement(ObjectRepository.SubmitById).click();
+        driver.findElement(ObjectRepository.LogoutById).click();
+
+
+
+        //Unable to use properties file (I DON'T KNOW WHY!!!)
+
+        /*driver.findElement(By.name(pro.getProperty("demo.login.username"))).sendKeys("admin");
+        driver.findElement(By.name(pro.getProperty("demo.login.password"))).sendKeys("demo");
+        driver.findElement(By.id(pro.getProperty("demo.login.submit"))).click();*/
 
     }
 
